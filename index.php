3b274993f55454c76fcb3b59ce0263296ebc90b4
Gokhan Cali <124929135+gokhancali0671@users.noreply.github.com>
2025-03-01 13:06:57 +0300

    diger dosyalar

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php wp_header(); ?>
+<body>
+
+<header>
+    <h1><?php bloginfo('name'); ?></h1>
+    <p><?php bloginfo('description'); ?></p>
+</header>
+
+<main>
+    <h2>Hi!</h2>
+</main>
+
+<?php wp_footer(); ?>
+</body>
+</html>
